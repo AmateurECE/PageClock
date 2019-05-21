1fcd50c000fd90707f371b941c67e6305b28dbab
AmateurECE <edtwardy@mtu.edu>
2019-05-20 23:54:06 -0400

    Abstract away Timer class, update popup
    This patch removes the Timer logic from the PageClock class and
    instead places it in its own class. Also add a couple of elements and
    event listeners to the popup.

--- background.js
+++ background.js
@@ -11,33 +11,26 @@
 ////
 
 ///////////////////////////////////////////////////////////////////////////////
-// Class: PageClock
+// Class: Timer
 ///
 
-function PageClock(matches) {
-
+function Timer() {
     // Instance attributes
-    this.matches = matches;
-    this.url = null;
-    this.startTime = new Date(0);
-    this.debug = false;
     this.running = false;
+    this.startTime = new Date(0);
+    this.time = new Date(0);
+    this.debug = false;
 
-    // Getters and Setters
-    this.setUrl = function(url) { this.url = url; }
-    this.getUrl = function()    { return this.url; }
-
+    this.getDebug = function() { return this.debug; }
     this.setDebug = function(debug) { this.debug = debug; }
-    this.getDebug = function()      { return this.debug; }
-
-    this.timerIsRunning = function()    { return this.running; }
+    this.isRunning = function()    { return this.running; }
     // TODO: Update getTime
     this.getTime = function()           { return this.time; }
-    this.reset = function()             { this.time = new Date(0); }
+    this.reset = function()             { this.time.setTime(0); }
 
     // Read/Initialize the current time variable from app storage
-    this.readTime = function() {
-        chrome.storage.sync.get(['PageClock.time'], (time) => {
+    this.readTime = function(name) {
+        chrome.storage.sync.get([name], (time) => {
             var self = this;
             if (typeof chrome.runtime.lastError !== 'undefined') {
                 console.error(chrome.runtime.lastError);
@@ -47,13 +40,10 @@ function PageClock(matches) {
             }
         })
     }
-    // TODO: this.readTime()
-    // this.readTime(); // Initialize the time attribute at construction time.
-    this.time = new Date(0);
 
     // Write/Save the current time variable into app storage
-    this.writeTime = function() {
-        chrome.storage.sync.set({'PageClock.time': this.time}, () => {
+    this.writeTime = function(name) {
+        chrome.storage.sync.set({name: this.time}, () => {
             if (typeof chrome.runtime.lastError !== 'undefined') {
                 console.error(chrome.runtime.lastError);
             }
@@ -61,7 +51,7 @@ function PageClock(matches) {
     }
 
     // Start the timer (save the current date as the start time)
-    this.startTimer = function() {
+    this.start = function() {
         this.startTime = new Date();
         if (this.debug) {
             console.log('Starting timer');
@@ -70,7 +60,7 @@ function PageClock(matches) {
     }
 
     // Stop the timer (increment this.time by the time that's elapsed)
-    this.stopTimer = function() {
+    this.stop = function() {
         // Stop the timer and update the time
         var diffTime = new Date(new Date() - this.startTime);
         this.time.setTime(this.time.getTime() + diffTime.getTime());
@@ -80,22 +70,50 @@ function PageClock(matches) {
         }
         this.running = false;
     }
+}
 
-    // Update the timer
+///////////////////////////////////////////////////////////////////////////////
+// Class: PageClock
+///
+
+function PageClock(matches) {
+    // Instance attributes
+    this.matches = matches;
+    this.url = null;
+    this.debug = false;
+    this.timer = new Timer();
+
+    // TODO: this.readTime()
+    // this.readTime(); // Initialize the time attribute at construction time.
+
+    // Getters and Setters
+    this.getUrl = function()    { return this.url; }
+    this.setUrl = function(url) { this.url = url; }
+
+    this.getDebug = function()      { return this.debug; }
+    this.setDebug = function(debug) {
+        this.debug = debug;
+        this.timer.setDebug(debug);
+    }
+
+    this.getMatches = function()        { return this.matches; }
+    this.setMatches = function(matches) { this.matches = matches; }
+
+    this.getTime = function() { return this.timer.getTime(); }
+    this.timerIsRunning = function() { return this.timer.isRunning(); }
+    this.resetTimer = function() { this.timer.reset(); }
+
+    // Update the timer when a new page loads
     this.update = function(url) {
         var self = this;
-        if (url == self.url) {
-            return;
-        }
-
-        self.url = url;
+        self.url = url; // Update the URL
         if (self.debug) {
             console.log('New URL: ' + url);
         }
 
         // Stop timer, if it is running
-        if (self.timerIsRunning()) {
-            self.stopTimer();
+        if (self.timer.isRunning()) {
+            self.timer.stop();
         }
 
         // Determine if we also need to start the timer
@@ -105,7 +123,7 @@ function PageClock(matches) {
                 if (self.debug) {
                     console.log('Matches: ' + element);
                 }
-                self.startTimer();
+                self.timer.start();
             }
         })
     }
@@ -114,17 +132,6 @@ function PageClock(matches) {
 ///////////////////////////////////////////////////////////////////////////////
 // Main
 ///
-
-// Called whenever:
-// - The Tab object experiences an update to its attributes
-// - A different Tab becomes active.
-function updatedListener(tabId, changeInfo, tab) {
-    chrome.tabs.query({"active": true},
-                      function(tabs) {
-                          // TODO: Check that tabs has exactly one entry.
-                          thePageClock.update(tabs[0].url);
-                      });
-}
 
 // Called when the background script is installed. Initializes thePageClock.
 var thePageClock = null;
@@ -137,6 +144,19 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log('Installed PageClock v'
                 + chrome.runtime.getManifest().version);
 });
+
+// Called whenever:
+// - The Tab object experiences an update to its attributes
+// - A different Tab becomes active.
+function updatedListener(tabId, changeInfo, tab) {
+    chrome.tabs.query({"active": true},
+                      function(tabs) {
+                          // TODO: Check that tabs has exactly one entry.
+                          if (tabs[0].url != thePageClock.getUrl()) {
+                              thePageClock.update(tabs[0].url);
+                          }
+                      });
+}
 
 // Set up event listeners.
 // TODO: Run when browser is closed?
--- popup.js
+++ popup.js
@@ -1,19 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            popup.js
+//
+// AUTHOR:          Ethan D. Twardy <edtwardy@mtu.edu>
+//
+// DESCRIPTION:     
+//
+// CREATED:         05/20/2019
+//
+// LAST EDITED:     05/20/2019
+////
+
 function startTime() {
     chrome.runtime.getBackgroundPage((backPage) => {
-        var time = backPage.thePageClock.getTime();
+        var pageClock = backPage.thePageClock;
+        var time = pageClock.getTime();
         printTime(time);
         // Update the time every second, so it looks like we're continuously
         // counting
-        setInterval(() => {
-            time = new Date(time.getTime() + 1000)
-            printTime(time);
-        }, 1000);
+        if (pageClock.timerIsRunning()) {
+            setInterval(() => {
+                time = new Date(time.getTime() + 1000)
+                printTime(time);
+            }, 1000);
+        }
     });
 }
 
 function printTime(time) {
+    // The Unix Epoch starts at 1900 hours, hence we subtract 19 hours.
     document.getElementById('time').innerHTML =
-        zeroExtend(time.getHours()) + 'h '
+        zeroExtend(time.getHours() - 19) + 'h '
         + zeroExtend(time.getMinutes()) + 'm '
         + zeroExtend(time.getSeconds()) + 's';
 }
@@ -23,4 +39,36 @@ function zeroExtend(i) {
     return i;
 }
 
+function startTextarea() {
+    chrome.runtime.getBackgroundPage((backPage) => {
+        var pageClock = backPage.thePageClock;
+        document.getElementById('textarea').innerHTML = pageClock.getMatches();
+    });
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Main
+///
+
 startTime();
+startTextarea();
+let updateButton = document.getElementById('update');
+updateButton.addEventListener('click', function(element) {
+    chrome.runtime.getBackgroundPage((backPage) => {
+        var pageClock = backPage.thePageClock;
+        var textInput = document.getElementById('textarea').innerHTML;
+        pageClock.setMatches(textInput.split('\n'));
+        // TODO: Trigger update when form input
+        // TODO: Start incrementing page's counter, if not already.
+    });
+});
+
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', function(element) {
+    chrome.runtime.getBackgroundPage((backPage) => {
+        var pageClock = backPage.thePageClock;
+        pageClock.resetTimer();
+    });
+});
+
+///////////////////////////////////////////////////////////////////////////////
